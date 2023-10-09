166065069d9e031ecd96f7c572164b5369789bdc
CLAUDIO LEAL <107277792+ClaudioLinsLeal89@users.noreply.github.com>
2023-10-09 10:06:11 -0300

    Atual09102023

--- Streamlittexto.py
+++ Streamlittexto.py
@@ -1,8 +1,25 @@
 import streamlit as st
 
 st.title('FLUXO DE CAIXA')
-st.header("Tomada de decisão")
-st.subheader("Economias e Estouros")
+st.header("TOMADA DE DECISÃO")
+st.subheader("ECONOMIAS E ESTOUROS")
 
-st.markdown("O fluxo de caixa é uma ferramenta fundamental para tomada de decisaão e acompanhamento do caixa")
+st.markdown("Importância da assertividade das obrigações a pagar para o efeito ao caixa")
+
+st.caption("este é  o captiom")
+
+# Code
+
+code = '''if(fome > 0)
+      return "ir para geladeira"
+else: 
+      return "estudar Streamlit"'''
+st.code(code,language='python')
+
+st.text('este é um texto usando st.texto')
+
+# latex https://katex.org/docs/support_table
+
+st.latex("\begin{align}a&=b+c\\d+e&=f\end{align}")
+
 
